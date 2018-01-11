1e5237f8dc0dfb8169a0b294ef7169d6f99503de
Scott <socialflamingo@scotts-imac.local>
2018-01-11 18:18:24 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Scott on 1/11/18.
+//  Copyright © 2018 Scott Bernardez. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
